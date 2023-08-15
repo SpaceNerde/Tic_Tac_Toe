9334eef4d424d2095026198df65c9bcd912206da
Space Nerd <florianpospiech77@gmail.com>
2023-08-15 22:47:28 +0200

    Add a player

--- tic_tac_toe_text_based.py
+++ tic_tac_toe_text_based.py
@@ -10,9 +10,23 @@ ttt_map = ([[" ", "|", " ", "|", " "],  # 1:1 | 3:1 | 5:1
             ["_", "+", "_", "+", "_"],  #
             [" ", "|", " ", "|", " "]]) # 1:5 | 3:5 | 5:5
 
+def get_player():
+    player = input("Please Choose if you are X or O\n"
+                   ">> ")
+    return player
+
 def run():
     print('\n'.join([''.join(['{:4}'.format(item) for item in row])
       for row in ttt_map]))
+
+    print("\nThis is the state of the board\n")
+
+    player = get_player()
+    if not player in ("O", "X"):
+        print("\nYou have to choose between X and O")
+        player = get_player()
+
+    print("\nYou are Player " + player)
 
 def print_coordination_data(x, y):
     if not isinstance(x and y, int):
